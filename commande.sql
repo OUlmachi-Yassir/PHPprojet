6c9996d78c725dc1b5d0052e563981062e776557
OUlmachi-Yassir <144781400+OUlmachi-Yassir@users.noreply.github.com>
2023-12-03 21:42:02 +0100

    Update commande.sql

--- commande.sql
+++ commande.sql
@@ -30,16 +30,12 @@ add projet int(10),
 FOREIGN KEY (projet) REFERENCES projet(id_pro);
 
 
-insert into projet VALUES (1,'Travigo travel','amelioration d'un site web de voyage'),(2,'Restaurant pizza','maquetter et mettre en oeuvre site web de pizza restaurant'),(3,'Salle de sport','realiser et implementer un site web de salle de sport'),(4,'Gamebit','concevoir un site d'une société de gaming'),(5,'Datware','gerer le personnel de l'entreprise dataware');
+insert into projet VALUES (1,'DATAware','make your first web site using php and localhost'),(2,'super pizza','learning html css by practicing it by making a restaurant  site web '),(3,'WWP','making sit web using js and frameworke css');
 
-insert into equipe VALUES (1,'codecrafters','2023-5-10'),(2,'nightcrawlers','2023-7-9'),(3,'thefive','2023-10-7'),(4,'codex','2023-11-23'),(5,'ventures','2023-10-11');
+insert into equipe VALUES (1,'skylanders','2023-5-10'),(2,'nightcrawlers','2023-7-9'),(3,'faceless','2023-10-7');
 
 INSERT INTO utilisateur (id,nom,prenom,email,pass,tel,statut,role) VALUES
-(1,'Sebti','Douae','douaesb123@gmail.com','douae123',0664589784,'active','ProductOwner'),
-(2,'OLM','Yassir','yassirolm123@gmail.com','yassir123',0615878477,'active','membre'),
-(3,'Toto','Mouad','mouadtoto123@gmail.com','Mouad123',0687459165,'active','membre'),
-(4,'Houas','Chaimae','chaimaeh123@gmail.com','chaimae123',0684516578,'active','membre'),
-(5,'Daali','Mohamed','mohamedda123@gmail.com','Mohamed123',0616457899,'active','membre');
+
 
 
 
